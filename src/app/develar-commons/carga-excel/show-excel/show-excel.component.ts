a0d1aa193c7db912f74e929a134bf73c923e7c8e
milton-sesarego <milton.sesarego@gmail.com>
2020-08-06 18:16:59 -0300

    commit

--- src/app/develar-commons/carga-excel/show-excel/show-excel.component.ts
+++ src/app/develar-commons/carga-excel/show-excel/show-excel.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Asset } from '../../develar-entities';
+import * as Excel from 'exceljs'
 
 @Component({
   selector: 'show-excel',
@@ -6,10 +8,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./show-excel.component.scss']
 })
 export class ShowExcelComponent implements OnInit {
+  @Input() asset: Asset;
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  readExcel(event) {
+        const workbook = new Excel.Workbook();
+        const target: DataTransfer = <DataTransfer>(event.target);
+        if (target.files.length !== 1) {
+          throw new Error('Cannot use multiple files');
+        }
+
+        /**
+         * Final Solution For Importing the Excel FILE
+         */
+
+        const arryBuffer = new Response(target.files[0]).arrayBuffer();
+        arryBuffer.then(function (data) {
+          workbook.xlsx.load(data)
+            .then(function () {
+
+              // play with workbook and worksheet now
+              console.log(workbook);
+              const worksheet = workbook.getWorksheet(1);
+              console.log('rowCount: ', worksheet.rowCount);
+              worksheet.eachRow(function (row, rowNumber) {
+                console.log('Row: ' + rowNumber + ' Value: ' + row.values);
+              });
+
+            });
+        });
+      }
 
 }
